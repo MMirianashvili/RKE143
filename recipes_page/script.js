3420c28cd2a9ad3145e47eef40bacc40973d1cc5
MMirianashvili <mykhailo.mirianashvili@tktk.ee>
2026-05-14 19:24:48 +0300

    added cors

--- recipes_page/script.js
+++ recipes_page/script.js
@@ -1,29 +1,26 @@
-const recipeCard = document.getElementById("recipe-card");
-const button = document.getElementById("new-recipe-btn");
+async function getRandomRecipe() {
 
-async function getRecipe() {
-
-    const response = await fetch(
-       const API_URL = "https://rke143-9tz6.onrender.com/random";
-    );
+    const response = await fetch("https://rke143-9tz6.onrender.com/random");
 
     const recipe = await response.json();
 
-    recipeCard.innerHTML = `
+    console.log(recipe);
+
+    document.getElementById("recipeContainer").innerHTML = `
         <h2>${recipe.name}</h2>
 
-        <img src="${recipe.imageURL}" alt="${recipe.name}">
+        <img src="${recipe.imageURL}" width="300">
 
         <h3>Ingredients:</h3>
 
         <ul>
-            ${recipe.ingredients
-                .map(item => `<li>${item}</li>`)
-                .join("")}
+            ${recipe.ingredients.map(item => `<li>${item}</li>`).join("")}
         </ul>
     `;
 }
 
-button.addEventListener("click", getRecipe);
+getRandomRecipe();
 
-getRecipe();
+document.getElementById("newRecipeBtn").addEventListener("click", () => {
+    getRandomRecipe();
+});
--- server.js
+++ server.js
@@ -38,6 +38,11 @@ function getRandomRecipe(recipes) {
 }
 
 const server = http.createServer((req, res) => {
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
     const url = req.url;
     const method = req.method;
 
